7b65fcc81bb48c9d2a1fc8646c2663db25baae04
Ruslan-Ruslan-Ruslan <lashkoruslan@gmail.com>
2024-09-08 21:31:46 +0300

    Initial commit: added HelloWorld program

---
+++ HelloWorldDemo.java
@@ -0,0 +1,6 @@
+public class HelloWorldDemo {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+        System.out.println("This task done by Lashko Ruslan, group PZ-21-1/9");
+    }
+}
---
+++ public class HelloWorldDemo {.java
@@ -0,0 +1,6 @@
+public class HelloWorldDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This task done by Ivan Ivanov, group KI-21");
+    }
+}
