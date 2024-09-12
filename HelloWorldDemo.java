6ef8c15bd8b83a818afd7c40a6e657937297cf4b
Ruslan-Ruslan-Ruslan <lashkoruslan@gmail.com>
2024-09-12 10:56:28 +0300

    Updated HelloWorldDemo with current date output

--- HelloWorldDemo.java
+++ HelloWorldDemo.java
@@ -1,6 +1,11 @@
+import java.time.LocalDate;
+
 public class HelloWorldDemo {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("This task done by Lashko Ruslan, group PZ-21-1/9");
+        
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Current date: " + currentDate);
     }
 }
--- public class HelloWorldDemo {.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorldDemo {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("This task done by Ivan Ivanov, group KI-21");
-    }
-}
